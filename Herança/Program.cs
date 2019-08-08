e2ce18715c0d473d55e1088fd9e7c0945c6bd245
Bruno Amaral <bruno_henrique_a@hotmail.com>
2019-08-08 14:14:08 -0300

    Projeto banco com uso de herança

---
+++ Herança/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Herança.Entities;
+
+namespace Herança
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account acc = new Account(1001, "Bruno", 0.0);
+            BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.0);
+
+            //upcasting
+
+            Account acc1 = bacc;
+            Account acc2 = new BusinessAccount(1003, "Bob", 0.0, 200.0);
+            Account acc3 = new SavingsAccount(1004, "Anna", 0.0, 0.01);
+
+            //downcasting
+
+            BusinessAccount acc4 = (BusinessAccount)acc2;
+            acc4.Loan(100.0);
+            
+
+            // caso sejam tipos incompativeis fazer o teste
+
+            if (acc3 is BusinessAccount)
+            {
+                BusinessAccount acc5 = (BusinessAccount)acc3;
+                acc5.Loan(200.0);
+                Console.WriteLine("Loan!");
+            }
+
+            if (acc3 is SavingsAccount)
+            {
+                SavingsAccount acc5 = (SavingsAccount)acc3;
+                acc5.UpdateBalance();
+                Console.WriteLine("Update!");
+            }
+
+
+
+        }
+    }
+}
